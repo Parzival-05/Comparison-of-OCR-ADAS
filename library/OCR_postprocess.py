d6a35de7bd6466133412f46f1ed7b7d7a14377c8
Parzival-05 <parzivalwasd@gmail.com>
2023-12-16 15:34:40 +0300

    feat(OCR_postprocess): Implement OCR postprocessing

---
+++ library/OCR_postprocess.py
@@ -0,0 +1,19 @@
+class OCRPostprocess:
+    def __init__(self, GTD: list[list[str]]):
+        self.chars = set()
+        for words in GTD:
+            for word in words:
+                for char in word.lower():
+                    self.chars.update(char)
+        print(self.chars)
+
+    def remove_excess_chars(self, string: str):
+        return "".join(char for char in string if char in self.chars)
+
+    def postprocess(self, result: list[str]):
+        postprocessed = []
+        for string in result:
+            postprocessed_string = self.remove_excess_chars(string)
+            if postprocessed_string:
+                postprocessed.append(postprocessed_string)
+        return postprocessed
